98e310895dd9b35956f349df98b4d832c059fdca
Скакун МА <manjasha_lee@mail.ru>
2019-07-22 17:03:50 +0600

    factorial

--- 3/src/lesson2.java
+++ 3/src/lesson2.java
@@ -1,39 +1,23 @@
 public class lesson2 {
     public static void main(String[] args) {
-     int[] ints = new int[]{11,12,13,14,15,16,17,18};
-        int i=0;
+        factorial(2);
 
-        int[] cifry = new int[]{11,12,13,14,15,16,17,18};
 
-        do {
-            i++;
-            System.out.println(cifry[i]);
+    }
+    public static void factorial(int b){
+        int[] num = new int[b+1];
+        int i;
+        int f;
+    for (i=1;i>b;i++){
+        f = i*i++;
+        System.out.println(f);
+        for (int i1 : num) {
+            System.out.println(f);
         }
-        while (cifry[i]<15);
-
-        polymorphism (2,3);
-        polymorphism(8);
-        polymorphism (1,2,3);//проверка приоритета логической операции
-
-    }
-    static void polymorphism (int b, int c){
-        int d = b+c;
-        System.out.println(d);
     }
 
-    static void polymorphism (int b){
-        int d = b*b;
-        System.out.println(d);
     }
 
-    static void polymorphism (int b, int c, int e){
-        int d = b+c*e;
-        System.out.println(d);
-    }
-
-
-
-
-    }
+}
 
 
