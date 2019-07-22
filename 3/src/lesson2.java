6edc90d64a0e888ecf041ae63d75210909f54ca0
Скакун МА <manjasha_lee@mail.ru>
2019-07-22 11:44:32 +0600

    fignya s ciklami

---
+++ 3/src/lesson2.java
@@ -0,0 +1,19 @@
+public class lesson2 {
+    public static void main(String[] args) {
+     int[] ints = new int[]{11,12,13,14,15,16,17,18};
+        int i=0;
+
+
+        while (ints[i]>15){
+            ints[i]=i+10;
+            System.out.println(ints[i]);
+        }
+        for (int anInt : ints) {
+            System.out.println(ints[i]);
+        }
+
+
+        }
+    }
+
+
