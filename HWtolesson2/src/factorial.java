43f87a1f9bd7ea018e858c72ef6f561433e7f4f7
Скакун МА <manjasha_lee@mail.ru>
2019-08-02 13:19:09 +0600

    done factorial to 12!

--- HWtolesson2/src/factorial.java
+++ HWtolesson2/src/factorial.java
@@ -2,13 +2,13 @@ public class factorial {
 
         public static void main(String[] args) {
             int n;
-            n = fact(3);
+            n = fact(8);
             System.out.println(n);
         }
         static int fact (int var){
             int result = 1;
-            for (int i = 1; i <= (var-1); i++) {
-                result = result*(var -(var - (i + 1)));
+            for (int i = 1; i < var; i++) {
+                result = result*(var-(var-(i+1)));
             }
             return result;
         }
