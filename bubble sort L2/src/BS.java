9df24d8251a35e00ddeeae1d19bc96e7903bded0
Скакун МА <manjasha_lee@mail.ru>
2019-08-14 16:41:41 +0600

    bubble sort and binary search

--- bubble sort L2/src/BS.java
+++ bubble sort L2/src/BS.java
@@ -1,25 +1,41 @@
 
-    public class BS {
-        public static void main(String[] args) {
-            int[] num = new int []{2,3,5,4,1};
+public class BS {
+    public static void main(String[] args) {
 
-            for (int i = 0; i < (num.length-1); i++) {
-                if (num[i]<num[i+1]){
-                    System.out.println(num[i]);
-                }else {
-                    sort(num);
-                    i = -1;
-                }
-            }
+        int[] num = new int[]{2, 3, 5, 4, 1, 6, 9, 10, 8, 7};
 
-        }
-        static void sort(int[]num1){
-            for (int i = 0; i<(num1.length-1); i++) {
-                if(num1[i]>(num1[i+1])){
-                    int var = num1[i];
-                    num1[i] = num1[i+1];
-                    num1[i+1] = var;
-                }
+        sort(num);
+        System.out.println(search(num, 11));
+    }
+
+    static void sort(int[] num2) {
+        for (int i = 0; i < (num2.length - 1); i++) {
+            if (num2[i] < num2[i + 1]) {
+                System.out.println(num2[i]);
+            } else {
+                int var = num2[i];
+                num2[i] = num2[i + 1];
+                num2[i + 1] = var;
+                i = -1;
             }
         }
+    }
+
+   public static int search (int[] array, int b) {
+        int min = 0;
+        int max = array.length - 1;
+
+        while (min <= max) {
+            int point = min + (max - min) / 2;
+            if (b < array[point]) {
+                max = point - 1;
+            } else if (b > array[point]) {
+                min = point + 1;
+            } else {
+                return point;
+            }
+        }
+        return-1;
+    }
 }
+
