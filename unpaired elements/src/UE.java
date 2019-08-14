6d8d27c7b03d251674808e79f2cd6922c4b24e51
Скакун МА <manjasha_lee@mail.ru>
2019-08-14 17:31:33 +0600

    new

---
+++ unpaired elements/src/UE.java
@@ -0,0 +1,31 @@
+public class UE {
+    public static void main(String[] args) {
+        int[]array = new int[]{1, 2, 3, 1, 3, 2, 3};
+        sort(array);
+        System.out.println(unPaired(array));
+    }
+
+    static void sort(int[] num) {
+        for (int i = 0; i < (num.length - 1); i++) {
+            if (num[i] < num[i + 1]) {
+                System.out.println(num[i]);
+            } else {
+                int var = num[i];
+                num[i] = num[i + 1];
+                num[i + 1] = var;
+                i = -1;
+            }
+        }
+    }
+
+    static int unPaired (int[]arr){
+        for (int i = 0; i < arr.length; i+=2) {
+
+            if (arr[i] == arr[i + 1]) {
+            }else {
+             return arr[i];
+            }
+        }
+        return -1;
+    }
+}
