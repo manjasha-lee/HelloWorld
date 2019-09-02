650de02d066a44b065fa5950e56cff499089bfe2
Скакун МА <manjasha_lee@mail.ru>
2019-09-02 15:29:23 +0600

    unpaired element done

--- unpaired elements/src/UE.java
+++ unpaired elements/src/UE.java
@@ -1,15 +1,14 @@
 public class UE {
     public static void main(String[] args) {
-        int[]array = new int[]{1, 2, 3, 1, 3, 2, 3};
-
-        System.out.println(sort(array));
+        int[]array = new int[]{4, 2, 3, 4, 3, 2, 3};
+        sort(array);
         System.out.println(unPaired(array));
     }
 
     static void sort(int[] num) {
         for (int i = 0; i < (num.length - 1); i++) {
-            if (num[i] < num[i + 1]) {
-                System.out.println(num[i]);
+            if (num[i] < num[i + 1] || num[i] == num[i + 1]) {
+
             } else {
                 int var = num[i];
                 num[i] = num[i + 1];
@@ -24,7 +23,7 @@ public class UE {
 
             if (arr[i] == arr[i + 1]) {
             }else {
-             return arr[i];
+                return arr[i];
             }
         }
         return -1;
