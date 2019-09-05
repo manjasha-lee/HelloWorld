de7e060b9ccd93f66a4a7795b97baecaf3829d6a
Скакун МА <manjasha_lee@mail.ru>
2019-09-05 13:49:03 +0600

    unpaired element XOR done

--- unpaired elements/src/UE.java
+++ unpaired elements/src/UE.java
@@ -1,31 +1,15 @@
 public class UE {
     public static void main(String[] args) {
-        int[]array = new int[]{4, 2, 3, 4, 3, 2, 3};
-        sort(array);
+        int[]array = new int[]{1, 2, 3, 1, 3, 2, 3};
         System.out.println(unPaired(array));
     }
-
-    static void sort(int[] num) {
-        for (int i = 0; i < (num.length - 1); i++) {
-            if (num[i] < num[i + 1] || num[i] == num[i + 1]) {
-
-            } else {
-                int var = num[i];
-                num[i] = num[i + 1];
-                num[i + 1] = var;
-                i = -1;
+    static int unPaired (int[]arr) {
+        int i = 0;
+        int x = arr[i] ^ arr[i + 1];
+            for ( i = 1; i < (arr.length-1); i++) {
+                x = x ^ arr[i + 1];
             }
-        }
+        return x;
     }
 
-    static int unPaired (int[]arr){
-        for (int i = 0; i < arr.length; i+=2) {
-
-            if (arr[i] == arr[i + 1]) {
-            }else {
-                return arr[i];
-            }
-        }
-        return -1;
-    }
 }
