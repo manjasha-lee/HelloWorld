69364849b9ea0d071e1d74152a3c9142d4660639
Скакун МА <manjasha_lee@mail.ru>
2019-10-31 17:07:52 +0600

    домашнее задание к 3 уроку, add правильно сделала?

--- bubble sort L2/src/BS.java
+++ bubble sort L2/src/BS.java
@@ -2,41 +2,31 @@
 public class BS {
     public static void main(String[] args) {
         Cats cat1 = new Cats("Melissita", 3, "green");
-        Cats cat2 = new Cats("Timoshok", 13,"yellow");
-        Cats cat3 = new Cats ("Nyasha", 2, "hazel");
+        Cats cat2 = new Cats("Timoshok", 13, "yellow");
+        Cats cat3 = new Cats("Nyasha", 2, "hazel");
 
         System.out.println(cat1);
         System.out.println(cat2);
         System.out.println(cat3);
 
-        Dogs dog1 = new Dogs ("Feya", "Немецкая овчарка");
-        Dogs dog2 = new Dogs ("Feya", "Немецкая овчарка");
+        Dogs dog1 = new Dogs("Feya", "Немецкая овчарка");
+        Dogs dog2 = new Dogs("Feya", "Немецкая овчарка");
 
         System.out.println(dog1);
         System.out.println(dog2);
-        System.out.println(dog1==dog2);
+        System.out.println(dog1 == dog2);
 
         dog1 = dog2;
 
         System.out.println(dog1);
         System.out.println(dog2);
-        System.out.println(dog1==dog2);
+        System.out.println(dog1 == dog2);
 
-        LinkList one = new LinkList();
-        LinkList two = new LinkList();
-
-        one.setNum(1);
-        two.setNum(2);
-
-    }
-    static void getByNum (int num){
-        LinkList link;
-        int coun = 0;
-        mark: while(coun>-1) {
-            if(link.getNum(num).equels(coun))
-            System.out.println();
-        }
-
+        LinkList collection = new LinkList();
+        collection.add("первый");
+        collection.add("второй");
+        System.out.println(collection.get(0));
+        System.out.println(collection.get(1));
     }
 }
 
--- bubble sort L2/src/LinkList.java
+++ bubble sort L2/src/LinkList.java
@@ -1,16 +1,23 @@
 public class LinkList {
+    private String name;
     private int num;
+    int counter = 0;
 
-    public int getNum() {
-        return num;
+    public void add(String name){
+        LinkList x = new LinkList();
+        x.setNameAndNum(name, counter);
+        counter++;
     }
-
-    public void setNum(int num) {
+    public void setNameAndNum(String name, int num){
+        this.name = name;
         this.num = num;
+    }
+    public String get(int num){
+        return name;
     }
 
     @Override
-    public int hashCode() {
-        return super.hashCode();
+    public String toString() {
+        return name;
     }
 }
